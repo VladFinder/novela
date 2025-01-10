76e162f3cdab45689e92f966a56eadcd1b9003a0
VladFinder <56915940+VladFinder@users.noreply.github.com>
2025-01-10 07:04:56 +0500

    v 1.1
    Залил картинки, смотрим

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'test_novel';
+var cacheName = 'my_new_game';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
